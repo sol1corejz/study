8f96f464ed94574933632ff471accb5cc38fc756
Ilya Parunov <62014301+sol1corejz@users.noreply.github.com>
2021-03-07 20:16:19 +0800

    Update AudioDevice.java

--- Homework2/AudioDevice.java
+++ Homework2/AudioDevice.java
@@ -13,24 +13,4 @@ abstract public class AudioDevice {
     }
      abstract void playOn(String type);
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AudioDevice that = (AudioDevice) o;
-        return Objects.equals(type, that.type) && Objects.equals(song, that.song);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(type, song);
-    }
-
-    @Override
-    public String toString() {
-        return "AudioDevice{" +
-                "type='" + type + '\'' +
-                ", song=" + song +
-                '}';
-    }
 }
