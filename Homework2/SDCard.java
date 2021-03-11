62f65483235dd3596818576175f9d16e903585cb
Ilya Parunov <62014301+sol1corejz@users.noreply.github.com>
2021-03-11 19:34:28 +0800

    Delete Homework2 directory

--- Homework2/SDCard.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class SDCard extends RecordingDevice{
-    SDCard(Music ... songs){
-        super(songs);
-    }
-
-}
--- Homework2/VinylTurntable.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class VinylTurntable extends RecordingDevice{
-    VinylTurntable(Music ... songs){
-        super(songs);
-    }
-}
